8aac89fcbcdd1d83782fe8aae3b27e3c6a72b9b0
Mylonaki Christina <t8240088@aueb.gr>
2025-10-31 19:31:40 +0200

    FIRST TEST WITH MAVEN

---
+++ firsttest-app/src/main/java/gr/det/spinnovators/App.java
@@ -0,0 +1,13 @@
+package gr.det.spinnovators;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
