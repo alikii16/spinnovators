82abdc430fbca862326e810ac1c9b5cb56e39c2a
Mylonaki Christina <xrismil19@gmail.com>
2025-11-01 18:26:47 +0200

    Update App.java

--- OpenBudget-app/src/main/java/gr/det/spinnovators/App.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/App.java
@@ -1,13 +1,8 @@
 package gr.det.spinnovators;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class OpenBudgetApp 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
