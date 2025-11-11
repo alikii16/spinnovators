244e7551a563c00f58da7af27604b464864cfcf1
= <t8240052@aueb.gr>
2025-11-11 18:21:41 +0200

    Main Ministry File

--- FirstLogin.java
+++ FirstLogin.java
@@ -3,4 +3,4 @@ import java.util.Scanner;
 
 public class FirstLogin {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); }
