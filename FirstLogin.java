48a58f62596052a47447f65121b94ed3d68190b2
= <t8240052@aueb.gr>
2025-11-11 18:21:41 +0200

    Main Ministry File

---
+++ FirstLogin.java
@@ -0,0 +1,6 @@
+//This file will be used for users login
+import java.util.Scanner;
+
+public class FirstLogin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); }
