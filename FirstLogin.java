4ece536c99e499078091672f61fbb36f0b4759ca
alikii16 <t8240024@aueb.gr>
2025-11-13 13:08:10 +0200

    Delete FirstLogin.java

--- FirstLogin.java
+++
@@ -1,6 +0,0 @@
-//This file will be used for users login
-import java.util.Scanner;
-
-public class FirstLogin {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); }
