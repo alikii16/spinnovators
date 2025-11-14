b34cad68cce8b5035e749ef2541f45ca92c9fc2f
Mylonaki Christina <xrismil19@gmail.com>
2025-11-15 01:09:22 +0200

    Update EnvBudgetTranslator.java

--- OpenBudget-app/src/main/java/gr/det/spinnovators/EnvBudgetTranslator.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/EnvBudgetTranslator.java
@@ -1,1 +1,11 @@
+/*
+ * Τranslator class responsible for converting English JSON keys into their official Greek descriptions. 
+ * It reads the 'env_budget_translations.properties' file and provides a translation service
+*/
+package gr.det.spinnovators;
 
+
+public class EnvBudgetTranslator {
+
+  
+}
