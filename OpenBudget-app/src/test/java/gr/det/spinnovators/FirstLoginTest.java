37401dd44c372efd91e4c923013daa6b62e91652
alikii16 <t8240024@aueb.gr>
2025-11-18 21:29:40 +0200

    Corrected FirstLoginTest.java

--- OpenBudget-app/src/test/java/gr/det/spinnovators/FirstLoginTest.java
+++ OpenBudget-app/src/test/java/gr/det/spinnovators/FirstLoginTest.java
@@ -7,36 +7,44 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FirstLoginTest {
+
     @Test
     public void testMiisterLoginSuccess() {
         String simulatedInput = "Minister\nm1n1st3r\n";
-         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
-          ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-           System.setOut(new PrintStream(outputStream));
-            FirstLogin.login();
-             String output = outputStream.toString();
-              assertTrue(output.contains("Επιτυχής σύνδεση! Καλωσήρθατε κύριε Υπουργέ."),"Expected successful login message for Minister");
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+        
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        
+        FirstLogin.login();
+        String output = outputStream.toString();
+        assertTrue(output.contains("Επιτυχής σύνδεση! Καλωσήρθατε κύριε Υπουργέ."),"Expected successful login message for Minister");
     }
 
     @Test
     public void testEmployeeLoginSuccess() {
         String simulatedInput = "Employee\n3mpl0y33\n";
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
+        
         FirstLogin.login();
         String output = outputStream.toString();
-        assertTrue(output.contains("Επιτυχής σύνδεση! Καλωσήρθατε employeeUsername."),"Expected successful login message");
+        assertTrue(output.contains("Επιτυχής σύνδεση! Καλωσήρθατε Employee."),"Expected successful login message");
     }
 
     @Test
-    public void testWrongPassword() {
-        String simulatedInput = "Minister\nwrongpassword\nm1n1st3r\n";
+    public void testEmptyUsernameandPassword() {
+        String simulatedInput = "\n\nMinister\nm1n1st3r\n";
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
+
         FirstLogin.login();
         String output = outputStream.toString();
-        assertTrue(output.contains("Λάθος όνομα ή κωδικός. Προσπαθήστε ξανά."),"Expected wrong password message");
+        assertTrue(output.contains("Λάθος όνομα ή κωδικός. Προσπαθήστε ξανά."),"Expected wrong username/password message for empty input");
+        assertTrue(output.contains("Επιτυχής σύνδεση! Καλωσήρθατε κύριε Υπουργέ."),"Expected successful login message after retry");
     }
 }
