d2fd777db24c88e7ad1ccc85a9dac201fff00550
=Christina Mylonaki <xrismil19@gmail.com>
2025-11-21 18:11:29 +0200

    Style: Fix code style and Javadoc for EnvBudgetData.java

--- OpenBudget-app/src/main/java/gr/det/spinnovators/envdatamodel/EnvBudgetData.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/envdatamodel/EnvBudgetData.java
@@ -8,24 +8,29 @@ import java.util.Map;
 
 public class EnvBudgetData {
 
+  private final Map<String, EnvYear> dataByYear;
 
-    private final Map<String, EnvYear> dataByYear;
-
-    // Total budget per year (env_ministry_total_budget)
-    private final Map<String, Double> envMinistryTotalBudget;
-
-    // Constructor
-    public EnvBudgetData(Map<String, EnvYear> dataByYear, Map<String, Double> envMinistryTotalBudget) {
-        this.dataByYear = dataByYear;
-        this.envMinistryTotalBudget = envMinistryTotalBudget;
+  // Total budget per year (env_ministry_total_budget)
+  private final Map<String, Double> envMinistryTotalBudget;
+  // Constructor
+  
+  public EnvBudgetData(Map<String, EnvYear> dataByYear,
+                       Map<String, Double> envMinistryTotalBudget) {
+    this.dataByYear = dataByYear;
+    this.envMinistryTotalBudget = envMinistryTotalBudget;
+  }
+  
+  /* 
+   * javadoc.
+   */
+  public EnvYear getBudgetForYear(String year) {
+    if (dataByYear == null) {
+      return null;
     }
+    return dataByYear.get(year);
+  } // Returns EnvYear object for a specific year or null if it does not exist
 
-    public EnvYear getBudgetForYear(String year) {
-        if (dataByYear == null) return null;
-        return dataByYear.get(year);
-    } // Returns EnvYear object for a specific year or null if it does not exist
-
-    public Map<String, Double> getEnvMinistryTotalBudget() {
-        return envMinistryTotalBudget;
-    } // Getter for total budget per year
+  public Map<String, Double> getEnvMinistryTotalBudget() {
+    return envMinistryTotalBudget;
+  } // Getter for total budget per year
 }
