948fe8c8d146fc39deed0b39485f609d83680476
=Christina Mylonaki <xrismil19@gmail.com>
2025-11-21 18:12:36 +0200

    Style: Fix code style and Javadoc for EnvUnit.java

--- OpenBudget-app/src/main/java/gr/det/spinnovators/envdatamodel/EnvUnit.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/envdatamodel/EnvUnit.java
@@ -8,34 +8,35 @@ import java.util.List;
  */
 public class EnvUnit {
 
-    private final String jsonKey; // e.g. general_secretariat_for_energy
-    private final List<EnvEntry> entries;
+  private final String jsonKey; // e.g. general_secretariat_for_energy
+  private final List<EnvEntry> entries;
 
-    public EnvUnit(String jsonKey, List<EnvEntry> entries) {
-        this.jsonKey = jsonKey;
-	this.entries = entries;
+  public EnvUnit(String jsonKey, List<EnvEntry> entries) {
+    this.jsonKey = jsonKey;
+    this.entries = entries;
+  }
+
+  public String getJsonKey() {
+    return jsonKey;
+  }
+
+  public List<EnvEntry> getEntries() {
+    return entries;
+  }
+
+  /**
+   * Helper method: returns the EnvEntry with the given key,
+   * or null if it doesn't exist.
+   */
+  public EnvEntry getEntryByKey(String key) {
+    if (key == null) {
+      return null; //User's given key is null, might cause NullPointerException
     }
-
-    public String getJsonKey() {
-        return jsonKey;
+    for (EnvEntry entry : entries) {
+      if (entry.getJsonKey().equals(key)) {
+        return entry;
+      }
     }
-
-    public List<EnvEntry> getEntries() {
-        return entries;
-    }
-
-
-    /**
-     * Helper method: returns the EnvEntry with the given key,
-     * or null if it doesn't exist.
-     */
-    public EnvEntry getEntryByKey(String key) {
-        if (key == null) return null; //User's given key is null, might cause NullPointerException
-	for (EnvEntry entry : entries) {
-            if (entry.getJsonKey().equals(key)) {
-                return entry;
-            }
-        }
-        return null; //Entry not found
-    }
+    return null; //Entry not found
+  }
 }
