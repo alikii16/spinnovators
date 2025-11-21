52300a71a4b461b64a1c774d566ae63be1583cda
=Christina Mylonaki <xrismil19@gmail.com>
2025-11-21 18:12:00 +0200

    Style: Fix code style and Javadoc for EnvYear

--- OpenBudget-app/src/main/java/gr/det/spinnovators/envdatamodel/EnvYear.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/envdatamodel/EnvYear.java
@@ -8,51 +8,54 @@ import java.util.List;
 
 public class EnvYear {
 
-    private final String year;
-    private final List<EnvSector> sectors;
+  private final String year;
+  private final List<EnvSector> sectors;
 
-    public EnvYear(String year, List<EnvSector> sectors) {
-	    this.year = year;
-	    this.sectors = sectors;
+  public EnvYear(String year, List<EnvSector> sectors) {
+    this.year = year;
+    this.sectors = sectors;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public List<EnvSector> getSectors() {
+    return sectors;
+  }
+
+  /*
+   *  Traverses sectors --> units --> entries and returns the EnvEntry object.
+   */
+
+  public EnvEntry findEntry(String sectorKey, String unitKey, String entryKey) {
+    if (sectorKey == null || unitKey == null || entryKey == null) {
+      return null;
+    } // Might cause NullPointerException
+
+    // Finds the sector or returns null
+    EnvSector sector = getSectorByKey(sectorKey);
+    if (sector == null) {
+      return null;
     }
 
-    public String getYear() {
-        return year;
+    // Finds the unit inside the sector or returns null
+    EnvUnit unit = sector.getUnitByKey(unitKey);
+    if (unit == null) {
+      return null;
     }
 
-    public List<EnvSector> getSectors() {
-        return sectors;
+    // Sector and unit were found successfully, returns final EnvEntry object (the entry)
+    return unit.getEntryByKey(entryKey);
+  }
+
+  // Helper method: Searches Sector
+  private EnvSector getSectorByKey(String key) {
+    for (EnvSector sector : sectors) {
+      if (key.equals(sector.getJsonKey())) {
+        return sector;
+      }
     }
-
-    // Traverses sectors --> units --> entries and returns the EnvEntry object.
-    public EnvEntry findEntry(String sectorKey, String unitKey, String entryKey) {
-        if (sectorKey == null || unitKey == null || entryKey == null) {
-            return null;
-        } // Might cause NullPointerException
-
-	// Finds the sector or returns null
-        EnvSector sector = getSectorByKey(sectorKey);
-        if (sector == null) {
-            return null;
-        }
-
-        // Finds the unit inside the sector or returns null
-        EnvUnit unit = sector.getUnitByKey(unitKey);
-        if (unit == null) {
-            return null;
-        }
-
-        // Sector and unit were found successfully, returns final EnvEntry object (the entry)
-        return unit.getEntryByKey(entryKey);
-    }
-
-    // Helper method: Searches Sector
-      private EnvSector getSectorByKey(String key) {
-        for (EnvSector sector : sectors) {
-            if (key.equals(sector.getJsonKey())) {
-                return sector;
-            }
-        }
-        return null; // No match found
-    }
+    return null; // No match found
+  }
 }
