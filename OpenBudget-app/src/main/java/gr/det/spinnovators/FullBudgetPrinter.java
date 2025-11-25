75ca63c800a0d1744cd3180626188b95146faf9b
nikolasdristiliaris <t8240043@aueb.gr>
2025-11-25 13:34:02 +0200

    fix identation errors fullbudgetprinter4

--- OpenBudget-app/src/main/java/gr/det/spinnovators/FullBudgetPrinter.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/FullBudgetPrinter.java
@@ -22,3 +22,97 @@ public final class FullBudgetPrinter {
      * (e.g. 2023, 2024, 2025).
      */
 
+    public void showBudget(final String year) {
+        //checks if the characters are the same,
+        // not in the same diirection-string.
+        double totalBudget;
+
+        if ("2026".equals(year)) {
+
+            totalBudget = 0;
+
+            System.out.println("\n--- ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΕΤΟΥΣ " + year + "---");
+            int size = this.data.getSize26();
+            String[] names = this.data.getNames26();
+            double[] amounts = this.data.getBudgetAmount26();
+
+            for (int i = 0; i < size; i++) {
+                // modifications for display of colon, euro sign,
+                // correct display of amounts and line break
+                System.out.printf(
+                    "%s: %,.2f €\n", names[i], amounts[i]
+                );
+                totalBudget = totalBudget + amounts[i];
+            }
+
+            System.out.println("-------------------------");
+            System.out.printf(
+                "%s: %,.2f €\n", "ΣΥΝΟΛΙΚΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ", totalBudget
+            );
+
+        } else if ("2025".equals(year)) {
+
+            totalBudget = 0;
+
+            System.out.println("\n--- ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΕΤΟΥΣ " + year + "---");
+            int size = this.data.getSize25();
+            String[] names = this.data.getNames25();
+            double[] amounts = this.data.getBudgetAmount25();
+
+            for (int i = 0; i < size; i++) {
+                System.out.printf(" * %-55s: %,.2f €\n", names[i], amounts[i]);
+                totalBudget = totalBudget + amounts[i];
+            }
+
+            System.out.println("----------------------------");
+            System.out.printf(
+                " * %-55s: %,.2f €\n", "ΣΥΝΟΛΙΚΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ", totalBudget
+            );
+
+        } else if ("2024".equals(year)) {
+
+            totalBudget = 0;
+
+            System.out.println("\n--- ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΕΤΟΥΣ " + year + "---");
+            int size = this.data.getSize24();
+            String[] names = this.data.getNames24();
+            double[] amounts = this.data.getBudgetAmount24();
+
+            for (int i = 0; i < size; i++) {
+                System.out.printf(" * %-55s: %,.2f €\n", names[i], amounts[i]);
+                totalBudget = totalBudget + amounts[i];
+            }
+
+            System.out.println("----------------------------");
+            System.out.printf(
+                " * %-55s: %,.2f €\n", "ΣΥΝΟΛΙΚΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ", totalBudget
+            );    
+
+        } else if ("2023".equals(year)) {
+
+            totalBudget = 0;
+
+            System.out.println("\n--- ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΕΤΟΥΣ " + year + "---");
+            int size = this.data.getSize23();
+            String[] names = this.data.getNames23();
+            double[] amounts = this.data.getBudgetAmount23();
+
+            for (int i = 0; i < size; i++) {
+                System.out.printf(" * %-55s: %,.2f €\n", names[i], amounts[i]);
+                totalBudget = totalBudget + amounts[i];
+            }
+
+            System.out.println("----------------------------");
+            System.out.printf(
+                " * %-55s: %,.2f €\n", "ΣΥΝΟΛΙΚΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ", totalBudget
+            );
+
+        } else {
+
+            System.out.println("Δεν υπάρχουν δεδομένα για το έτος " + year);
+
+        }
+    }
+}
+
+
