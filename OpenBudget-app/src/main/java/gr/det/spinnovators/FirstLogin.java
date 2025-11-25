c19d0f4f83e52c3270dcb0d21fda5391445b909a
nikolasdristiliaris <t8240043@aueb.gr>
2025-11-25 13:03:47 +0200

    fix identation errors firstlogin #3

--- OpenBudget-app/src/main/java/gr/det/spinnovators/FirstLogin.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/FirstLogin.java
@@ -15,3 +15,23 @@ public final class FirstLogin {
      * Private constructor for utility class to prevent instantiation.
      */ // ΔΙΟΡΘΩΣΗ: Σωστή εσεντάτωση
 
+    private FirstLogin() {
+
+    }
+
+    /**
+     * It has to do with the login of the user,
+     * repeating it until connection.
+     */
+    public static void login() {
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        String minister = "Minister";
+        String passwordMinister = "m1n1st3r";
+        String passwordEmployee = "3mpl0y33";
+
+        boolean isValid;
+
+
+        do {
