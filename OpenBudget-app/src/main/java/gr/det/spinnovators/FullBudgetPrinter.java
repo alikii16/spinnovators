cb142be67b879e7bdd8830c8511c2b2ad8cdcb87
nikolasdristiliaris <t8240043@aueb.gr>
2025-11-25 13:31:32 +0200

    fix identation errors fullbudgetprinter3

--- OpenBudget-app/src/main/java/gr/det/spinnovators/FullBudgetPrinter.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/FullBudgetPrinter.java
@@ -5,3 +5,20 @@ public final class FullBudgetPrinter {
     /**
      *Creates variable data that receives the data from class MinistryDataInput.
      */
+    private MinistryDataInput data;
+
+    /**
+     * Constructor for FullBudgetPrinter.
+     * @param dataToUse The MinistryDataInput object containing budget
+     * data.
+     */
+    public FullBudgetPrinter(final MinistryDataInput dataToUse) {
+        this.data = dataToUse;
+    }
+
+    /**
+     * Displays the full bufget for the specified year.
+     * @param year The year for which to display the budget.
+     * (e.g. 2023, 2024, 2025).
+     */
+
