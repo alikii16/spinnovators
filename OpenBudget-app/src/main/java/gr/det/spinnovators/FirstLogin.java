3af7c450dcf4da3ecbb8ed5d0ab0e3388fcb137c
nikolasdristiliaris <t8240043@aueb.gr>
2025-11-25 13:03:19 +0200

    fix identation errors firstlogin #2

--- OpenBudget-app/src/main/java/gr/det/spinnovators/FirstLogin.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/FirstLogin.java
@@ -3,3 +3,15 @@ package gr.det.spinnovators;
 /**
  * This file will be used for users login.
  * */
+import java.util.Scanner;
+
+/**
+ * Utility class to handle user login process. 
+ * (Διορθώνει το MissingJavadocType στη γραμμή 5)
+ */
+public final class FirstLogin {
+
+    /**
+     * Private constructor for utility class to prevent instantiation.
+     */ // ΔΙΟΡΘΩΣΗ: Σωστή εσεντάτωση
+
