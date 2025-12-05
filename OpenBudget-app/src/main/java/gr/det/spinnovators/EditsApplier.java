77af13d53e5051c6d29b3d6c95696ea4dff9d9d7
nikolasdristiliaris <t8240043@aueb.gr>
2025-12-05 22:38:17 +0200

    change of function, add balance and check for ending

--- OpenBudget-app/src/main/java/gr/det/spinnovators/EditsApplier.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/EditsApplier.java
@@ -30,14 +30,31 @@ public class EditsApplier {
       double totalBudget = 3133452000.00;
     }
 
+    this.currentBalance = 0.0;
+    int counter = 0;
+
     while (keepEditing) {
+      if (counter == 0) {
+        counter = 3;
+      } else {
+        System.out.printf("ΥΠΟΛΟΙΠΟ ΓΙΑ ΙΣΟΣΚΕΛΙΣΜΟ: this.currentBalance");
+      }
+
       System.out.println("\\nΠληκτρολογήστε την κατηγορία που θέλετε να επεξεργαστείτε ή 'ΤΕΛΟΣ' για έξοδο.");
       System.out.print("-->");
       
       String searchInput = scanner.nextLine().trim();
 
       if (searchInput.equalsIgnoreCase("ΤΕΛΟΣ") || searchInput.equalsIgnoreCase("TELOS")) {
-        keepEditing = false;
+        if (Math.abs(this.currentBalance) == 0) {
+          System.out.println("O προυπολογισμός είναι ισοσκελισμένος!");
+          System.out.println("Τερματισμός Λειτουργίας");
+          keepEditing = false;
+        } else {
+          System.out.printf("Δεν επιτρέπεται τερματισμός");
+          System.out.printf("Ο προϋπολογισμός δεν ισοσκελίστηκε, συνεχίστε τις αλλαγές");
+          System.out.printf("Υπόλοιπο: ", currentBalance);
+        }
       } else if (!searchInput.isEmpty()) {
         findAndEditCategory(year, searchInput);
       }
@@ -71,12 +88,14 @@ public class EditsApplier {
               double newAmount = Double.parseDouble(amountInput);
               entry.setAmount(newAmount);
               double offsetAmount = oldAmount - newAmount;
+              //CURRENT BALANCE CORRECTION
+              this.currentBalance += offsetAmount;
               System.out.printf(" [OK] Η τιμή άλλαξε επιτυχώς. Δημιουργήθηκε διαφορά: ", offsetAmount, " ευρώ");
             } catch (NumberFormatException e) {
               System.out.println(" Λάθος: Παρακαλώ δώστε έγκυρο αριθμό.");
             }         
           return;
-          // EDO THA PERNAEI TON ELEGXO EGKYROTITAS KALONTAS THN METHODO CHRISANTHI/NIKOU
+          // EDO THA PERNAEI TON ELEGXO EGKYROTITA
         }
         }
       }
