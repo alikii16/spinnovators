f0f9a2906cb0c7b68d8d890af1d71945a393698a
nikolasdristiliaris <t8240043@aueb.gr>
2025-12-07 18:16:01 +0200

    CHECKSTYLE EDITSPRINTER IDENTATION 4

--- OpenBudget-app/src/main/java/gr/det/spinnovators/EditsPrinter.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/EditsPrinter.java
@@ -3,10 +3,10 @@ package gr.det.spinnovators;
 import gr.det.spinnovators.envdatamodel.EnvEntry;
 import gr.det.spinnovators.envdatamodel.EnvSector;
 import gr.det.spinnovators.envdatamodel.EnvUnit;
-import gr.det.spinnovators.envdatamodel.EnvYear;
 import gr.det.spinnovators.EnvBudgetTranslator;
+import gr.det.spinnovators.envdatamodel.EnvYear;
 
-/** PRINTS THE RESULTS OF CHANGED DATA */
+/** PRINTS THE RESULTS OF CHANGED DATA. */
 
 public class EditsPrinter {
   private final EnvBudgetTranslator translator;
@@ -17,45 +17,44 @@ public class EditsPrinter {
     this.yearForPrinting = yearForPrinting;
   }
 
-  /** PRINTS THE RESULTS OF THE CHANGED YEARS 
+  /** PRINTS THE RESULTS OF THE CHANGED YEARS. 
    * 
-   * @param year the ministry year data to print
+   * @param year the ministry year data to print.
    */
   public void printEditYear(EnvYear year) {
     if (Integer.parseInt(year.getYear()) != this.yearForPrinting) {
       System.out.println("Το έτος " + this.yearForPrinting
-          + "δεν ταιριάζει με το φορτωμένα δεδομένα");
+          + " δεν ταιριάζει με το φορτωμένα δεδομένα");
       return;
     }
 
     System.out.println("ΕΝΗΜΕΡΩΜΕΝΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΓΙΑ ΤΟ ΕΤΟΣ " + year.getYear());
+
 
     for (EnvSector sector : year.getSectors()) {
 
       String translatedSector = translator.translateCategory(sector.getJsonKey());
       System.out.printf("ΤΟΜΕΑΣ: %s%n", translatedSector);
       double sectorTotal = 0.0;
+      
+      for (EnvUnit unit : sector.getUnits()) {
+        String translatedUnit = translator.translateCategory(unit.getJsonKey());
+        System.out.printf("ΕΚΤΕΛΕΣΤΙΚΗ ΜΟΝΑΔΑ: %s%n", translatedUnit);
 
-        for (EnvUnit unit : sector.getUnits()) {
-          String translatedUnit = translator.translateCategory(unit.getJsonKey());
-          System.out.printf("ΕΚΤΕΛΕΣΤΙΚΗ ΜΟΝΑΔΑ: %s%n", translatedUnit);
+        double unitTotal = 0.0;
 
-          double unitTotal = 0.0;
-
-          for (EnvEntry entry : unit.getEntries()) {
-            String translatedEntry = translator.translateCategory(entry.getJsonKey());
-            double amount = entry.getAmount();
-
-            unitTotal += amount;
-          }
-
-          System.out.printf("ΣΥΝΟΛΟ ΜΟΝΑΔΑΣ: %.2f%n", unitTotal);
-          sectorTotal += unitTotal;
+        for (EnvEntry entry : unit.getEntries()) {
+          String translatedEntry = translator.translateCategory(entry.getJsonKey());
+          double amount = entry.getAmount();
+          unitTotal += amount;
         }
 
-        System.out.printf("ΣΥΝΟΛΙΚΟ ΠΟΣΟ ΤΟΜΕΑ %s: %.2f%n%n",
+        System.out.printf("ΣΥΝΟΛΟ ΜΟΝΑΔΑΣ: %.2f%n", unitTotal);
+        sectorTotal += unitTotal;
+      }
+      
+      System.out.printf("ΣΥΝΟΛΙΚΟ ΠΟΣΟ ΤΟΜΕΑ %s: %.2f%n%n",
             translatedSector, sectorTotal);
-
     }
   }
 }
