a3d4fe526b2d937d6acefd22263f29b6debddd67
nikolasdristiliaris <t8240043@aueb.gr>
2025-12-07 18:05:15 +0200

    CHECKSTYLE EDITSPRINTER IDENTATION 3

--- OpenBudget-app/src/main/java/gr/det/spinnovators/EditsPrinter.java
+++ OpenBudget-app/src/main/java/gr/det/spinnovators/EditsPrinter.java
@@ -11,47 +11,50 @@ import gr.det.spinnovators.EnvBudgetTranslator;
 public class EditsPrinter {
   private final EnvBudgetTranslator translator;
   private final int yearForPrinting;
-    
+
   public EditsPrinter(EnvBudgetTranslator translator, int yearForPrinting) {
     this.translator  = translator;
     this.yearForPrinting = yearForPrinting;
   }
 
-  /** PRINTS THE RESULTS OF THE CHANGED YEARS */
+  /** PRINTS THE RESULTS OF THE CHANGED YEARS 
+   * 
+   * @param year the ministry year data to print
+   */
   public void printEditYear(EnvYear year) {
     if (Integer.parseInt(year.getYear()) != this.yearForPrinting) {
       System.out.println("Το έτος " + this.yearForPrinting
           + "δεν ταιριάζει με το φορτωμένα δεδομένα");
       return;
     }
-    
+
     System.out.println("ΕΝΗΜΕΡΩΜΕΝΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΓΙΑ ΤΟ ΕΤΟΣ " + year.getYear());
-    
+
     for (EnvSector sector : year.getSectors()) {
 
       String translatedSector = translator.translateCategory(sector.getJsonKey());
-      System.out.printf("ΤΟΜΕΑΣ: %s", translatedSector);
+      System.out.printf("ΤΟΜΕΑΣ: %s%n", translatedSector);
       double sectorTotal = 0.0;
 
         for (EnvUnit unit : sector.getUnits()) {
           String translatedUnit = translator.translateCategory(unit.getJsonKey());
-          System.out.printf("ΕΚΤΕΛΕΣΤΙΚΗ ΜΟΝΑΔΑ: %s", translatedUnit);
+          System.out.printf("ΕΚΤΕΛΕΣΤΙΚΗ ΜΟΝΑΔΑ: %s%n", translatedUnit);
 
           double unitTotal = 0.0;
-          
+
           for (EnvEntry entry : unit.getEntries()) {
             String translatedEntry = translator.translateCategory(entry.getJsonKey());
             double amount = entry.getAmount();
 
             unitTotal += amount;
           }
-          
-          System.out.printf("ΣΥΝΟΛΟ ΜΟΝΑΔΑΣ: %s", unitTotal);
+
+          System.out.printf("ΣΥΝΟΛΟ ΜΟΝΑΔΑΣ: %.2f%n", unitTotal);
           sectorTotal += unitTotal;
         }
-        
-        System.out.printf("ΣΥΝΟΛΙΚΟ ΠΟΣΟ ΤΟΜΕΑ %s",
-              translatedSector, sectorTotal);
+
+        System.out.printf("ΣΥΝΟΛΙΚΟ ΠΟΣΟ ΤΟΜΕΑ %s: %.2f%n%n",
+            translatedSector, sectorTotal);
 
     }
   }
